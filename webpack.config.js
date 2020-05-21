e225801c314c0c7beede4b3c81d506b0bdccba2c
PhilippSchweizer <ph.m.schweizer@outlook.com>
2020-05-21 14:42:59 +0200

    Add and configure
    - Babel & babel-loader
    - file-loader
    - json-loader
    - HtmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,34 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/client/index.js'
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node-modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: '/\.(png|jpe?g|gif)$/i',
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    })
+  ]
 }
